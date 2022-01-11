13b64da041e5ed479e703369a1445f5c676559b2
Aman Jain <7793aman@gmail.com>
2022-01-11 12:51:25 -0800

    Comittingn update github  links

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -4,14 +4,14 @@ export const Projects = [
         name: "Portfolio Website",
         description: "My Portfolio website build on top of the React JS & Redux, with styledComponents and frmaer motion animations.",
         tags: ["react", "redux", "react-router", "framer-motion", "styledComponents"],
-        github: "https://github.com/codebucks27/Agency-website"
+        github: "https://github.com/7793aman/React-Portfolio-starter-code-files"
     },
     {
         id: 2,
         name: "Minesweeper",
         description: "A Minesweeper game created using vanilla Javascript",
         tags: ["Javascript"],
-        github: "https://github.com/codebucks27/react-job-listing-website"
+        github: "https://github.com/7793aman/MinesweeperJs"
 
     },
     {
@@ -19,29 +19,37 @@ export const Projects = [
         name: "Slack Clone",
         description: "A clone of Slack using ReactJS and Redux state management with Firebase realtime authenticaltion and database.",
         tags: ["react", "redux", "Firebase", "Flex-Box"],
-        github: "https://github.com/codebucks27/devto-clone"
+        github: "https://github.com/7793aman/slack-clone"
     },
     {
         id: 4,
         name: "Online Restraunt Website",
         description: "An online restraunt website created using Angular8, Bootstrap and Json Server ",
         tags: ["Angular8", "Bootstrap", "Json-server"],
-        github: "https://github.com/codebucks27/react-sidebar"
+        github: "https://github.com/7793aman/ConFusion"
 
     }, {
         id: 5,
         name: "Tesla Clone(React + Styled Components)",
         description: "A basic Tesla clone to learn the concept of React and Styled Components",
         tags: ["react", "redux", "framer-motion", "Flex-Box"],
-        github: "https://github.com/codebucks27/React-Redux-Todo-App"
+        github: "https://github.com/7793aman/tesla-clone"
 
     },
     {
         id: 6,
+        name: "Competitive programming files",
+        description: "A repo of various solved problems to learn the concept of Algorithms and Data Structures",
+        tags: ["C++", "Algorithms", "Data Structures"],
+        github: "https://github.com/7793aman/CP"
+
+    },
+    {
+        id: 7,
         name: "Netflix Clone",
         description: "In Progress",
         tags: ["Express.Js", "React", "Firebase", "MongoDb", "JWT"],
-        github: "https://github.com/codebucks27/React-responsive-navbar"
+        github: "https://github.com/7793aman/Netflix-Clone"
 
     },
 ] 
