417ea71a69801efeb743878cfd3d62e6d4aad2bc
Aman Jain <7793aman@gmail.com>
2022-01-12 10:49:09 -0800

    Making Logo clickable to home link

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -1,16 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import { DarkTheme } from "../components/Theme";
+import { NavLink } from "react-router-dom";
 
 const LogoComponent = (props) => {
     return (
-        <Logo color={props.theme} className="logo__container">
+        <Logo to={{ pathname: "/" }} color={props.theme} className="logo__container">
             AJ
         </Logo>
     )
 }
 
-const Logo = styled.h1`
+const Logo = styled(NavLink)`
+text-decoration:none;
+font-size:28px;
+font-weight:700;
 display:inline-block;
 color:${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
 font-family:'Pacifico',cursive;
