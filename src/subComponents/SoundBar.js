253daf7b087cf8dd3c45ca5d51e5945e1ff98ff2
Aman Jain <7793aman@gmail.com>
2022-01-11 17:00:32 -0800

    Updated the Sound Bar song

--- src/subComponents/SoundBar.js
+++ src/subComponents/SoundBar.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import styled, { keyframes } from "styled-components";
-import music from "../assets/audio/u-said-it-v13-1167.mp3"
+import music from "../assets/audio/Ludovico-Einaudi-Primavera.mp3"
 
 
 const SoundBar = (props) => {
